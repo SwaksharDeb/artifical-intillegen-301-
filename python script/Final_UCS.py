a60f95d138106e2d0dbf47bdbf63c5ebe3ef9f29
SwaksharDeb <49334830+SwaksharDeb@users.noreply.github.com>
2019-06-04 09:07:44 +0600

    Add files via upload

--- python script/Final_UCS.py
+++ python script/Final_UCS.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  8 22:32:47 2019
-
-@author: hp
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  8 16:35:39 2019
-
-@author: hp
-"""
 import csv
 from collections import defaultdict
 from queue import PriorityQueue
@@ -24,36 +11,70 @@ class Graph:
         return self.edges[node]
     
     def read_file(self):
-        f = open("ucs.csv",'r')
+        f = open("DFS.csv",'r')
         reader =csv.reader(f)                  #store file into 2D list. So reader is a 2D list
         self.edges = defaultdict(list)
         self.weights = defaultdict(int)
-        for row in reader:   
-            self.edges[row[0]].append(row[1])
-            self.weights[row[0]+row[1]] = row[2]
+        for row in reader:
+            no_child = row[1]
+            if no_child == '3':
+                self.edges[row[0]].append(row[2])
+                self.weights[row[0]+row[2]] = row[3]       #weights = local cost
+                self.edges[row[0]].append(row[4])
+                self.weights[row[0]+row[4]] = row[5]
+                self.edges[row[0]].append(row[6])
+                self.weights[row[0]+row[6]] = row[7]
+            if no_child == '2':
+                self.edges[row[0]].append(row[2])
+                self.weights[row[0]+row[2]] = row[3]       #weights = local cost
+                self.edges[row[0]].append(row[4])
+                self.weights[row[0]+row[4]] = row[5]
+               # self.hvalve[row[0]] = row[6]
+            if no_child =='1':
+                self.edges[row[0]].append(row[2])
+                self.weights[row[0]+row[2]] = (row[3])
+                #self.hvalve[row[0]] = (row[4])
+            #self.edges[row[0]].append(row[1])
+            #self.weights[row[0]+row[1]] = row[2]
 
     def get_cost(self, from_node, to_node):
         return int(self.weights[from_node + to_node])    # its return string. So we must convert it into corresponding integer
     
     
 def ucs(graph, start, goal):
+    li = []
     visited = set()
     queue = PriorityQueue()
+    dictonary = defaultdict(list)
     queue.put((0, start))
 
     while queue:
         cost, node = queue.get()
         if node not in visited:
-            print(node)
+            #print(node)
             visited.add(node)
-
-            if node == goal:
-                return
+            
             for i in graph.neighbors(node):
                 if i not in visited:
                     total_cost = cost + graph.get_cost(node, i)
+                    dictonary[(node,cost)].append([i,total_cost])
                     queue.put((total_cost, i))
+                    
+            if node == goal:
+                li.append(goal)
+                while goal != start:
+                    for key,value in dictonary.items():
+                        for i in range(0,len(value)):
+                            if value[i] == [goal,cost]:
+                                goal = key[0]
+                                cost = key[1]
+                                li.append(goal)
+                            
+                for i in reversed(li):
+                    print(i)
+                return
+
      
 graph = Graph()
 graph.read_file()
-ucs(graph,'0','5')
+ucs(graph,'S','G')
